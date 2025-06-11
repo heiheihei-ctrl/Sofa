b097fa92a51a8aa2942e516288f6c924522d553f
heiheihei-ctrl <208733909@qq.com>
2025-06-11 10:40:25 +0800

    first commit

---
+++ hydrogen.config.js
@@ -0,0 +1,9 @@
+// hydrogen.config.js
+export default {
+  logger: {
+    showQueryTiming: true, // 启用查询性能分析
+  },
+  devTools: { 
+    subrequestProfiler: true, // 确保启用 Subrequest Profiler
+  },
+};
